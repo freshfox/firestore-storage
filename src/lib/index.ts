08e90f8f8dc050a19d07c8da3dc5af3c07fbc846
Dominic Bartl <dominic.bartl.at@gmail.com>
2018-03-16 17:00:27 +0100

    Export all classes

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export * from './storage/base_model';
+export * from './storage/base_repository';
+export * from './storage/firestore_storage';
+export * from './storage/memory_storage';
+export * from './storage/migrations';
+export * from './storage/module';
+export * from './storage/storage';
