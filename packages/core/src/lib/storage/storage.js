d733364108e075b670f89a43dfe0d07cfbd9ba16
Dominic Bartl <dominic.bartl.at@gmail.com>
2020-10-09 16:50:37 +0200

    Remove compiled files

--- packages/core/src/lib/storage/storage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorFactory = exports.FirestoreInstance = exports.Storage = void 0;
-exports.Storage = Symbol('Storage');
-exports.FirestoreInstance = Symbol('FirestoreInstance');
-exports.ErrorFactory = Symbol('ErrorFactory');
-//# sourceMappingURL=storage.js.map
