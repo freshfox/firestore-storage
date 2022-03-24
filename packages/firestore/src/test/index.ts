8aad817aa9640405ebcb08fcc83fc0e4c30425c2
Dominic Bartl <dominic.bartl.at@gmail.com>
2022-03-24 14:09:10 +0100

    Add injectable to MemoryStorage

--- packages/firestore/src/test/index.ts
+++ packages/firestore/src/test/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
-import {Container, interfaces} from 'inversify';
-import {IStorageDriver, StorageDriver, BaseModel, ReferenceMap} from 'firestore-storage-core';
+import {Container, injectable, interfaces} from 'inversify';
+import {IStorageDriver, StorageDriver, BaseModel, ReferenceMap, MemoryStorage} from 'firestore-storage-core';
 import * as admin from 'firebase-admin';
 import * as env from 'node-env-file';
 import * as fs from 'fs';
@@ -11,6 +11,8 @@ const path = __dirname + '/../../.env';
 if(fs.existsSync(path)){
 	env(path);
 }
+
+Reflect.decorate([injectable() as any], MemoryStorage);
 
 export class TestFactory {
 
